d7b2aaaa60eb8f7b5d13e7870ecb2cfc8689e1ad
Karan Sheth <karan.sheth60@gmail.com>
2021-09-07 11:12:51 -0400

    Added Logo for GMB.

--- src/bento/globalHeaderData.js
+++ src/bento/globalHeaderData.js
@@ -1,8 +1,8 @@
 // globalHeaderLogo image 468x100
 // globalHeaderImage: image 2200x100
 export default {
-  globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/bento-tools/master/src/components/assets/header/CTDC_Logo.svg',
+  globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/gmb/images/logos/svgs/GMB_logo.svg',
   globalHeaderLogoLink: '/',
-  globalHeaderLogoAltText: 'Bento Logo',
+  globalHeaderLogoAltText: 'GMB Logo',
   globalHeaderImage: '',
 };
--- src/components/Header/HeaderView.js
+++ src/components/Header/HeaderView.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { Header } from 'bento-components';
 import headerData from '../../bento/globalHeaderData';
 
+const customStyle = {
+  nihLogoImg: {
+    width: '463px',
+    height: '100px',
+    marginLeft: '0px',
+    minHeight: '54px',
+    maxHeight: '100px',
+  },
+  headerBar: {
+    top: '0px',
+    zIndex: '999',
+  },
+};
+
 const ICDCHeader = () => (
   <>
     <Header
@@ -9,6 +23,7 @@ const ICDCHeader = () => (
       easter={headerData.globalHeaderImage}
       alt={headerData.globalHeaderLogoAltText}
       homeLink={headerData.globalHeaderLogoLink}
+      customStyle={customStyle}
     />
   </>
 );
