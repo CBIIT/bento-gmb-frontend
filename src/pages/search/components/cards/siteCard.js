7618d0b5fef03d7b48653574ad99d44e5305e2e4
ogwuazors <95305471+ogwuazors@users.noreply.github.com>
2022-04-27 12:57:36 -0400

    resolving issues with siteCard

--- src/pages/search/components/cards/siteCard.js
+++ src/pages/search/components/cards/siteCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, withStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { prepareLinks } from 'bento-components';
 import PropertyItem from '../propertyItem';
 
 const SiteCard = ({ data, classes, index }) => {
