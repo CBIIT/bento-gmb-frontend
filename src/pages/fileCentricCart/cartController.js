e154a0f26de94511bd4f847271d81373662d2525
ogwuazors <95305471+ogwuazors@users.noreply.github.com>
2022-04-27 12:53:57 -0400

    review issue resolution

--- src/pages/fileCentricCart/cartController.js
+++ src/pages/fileCentricCart/cartController.js
@@ -2,80 +2,31 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { getCart, updateSortOrder } from './store/cart';
 import { Typography } from '../../components/Wrappers/Wrappers';
-import { GET_MY_CART_DATA_QUERY, table } from '../../bento/fileCentricCartWorkflowData';
+import { GET_MY_CART_DATA_QUERY, GET_MY_CART_DATA_QUERY_DESC, table } from '../../bento/fileCentricCartWorkflowData';
 import CartView from './cartView';
 
-export default function cartController() {
+const cartController = () => {
   const cart = getCart();
-  const ids = cart.fileIds !== null || typeof cart.fileIds !== 'undefined' ? cart.fileIds : [];
-  const defaultSortDirection = cart.sortDirection === '' || !cart.sortDirection ? table.defaultSortDirection || 'asc' : cart.sortDirection;
-  const CART_QUERY = GET_MY_CART_DATA_QUERY;
-  const defaultSortColumnValue = cart.sortColumn === '' || !cart.sortColumn ? table.defaultSortField || '' : cart.sortColumn;
-
-  if (!localStorage.getItem('sortColumn') || !localStorage.getItem('page') || !localStorage.getItem('rowsPerPage')) {
-    localStorage.setItem('sortColumn', defaultSortColumnValue);
-    localStorage.setItem('page', '0');
-    localStorage.setItem('rowsPerPage', '10');
-  }
-
-  const localPage = localStorage.getItem('page');
-  const localRowsPerPage = localStorage.getItem('rowsPerPage');
-  /* const page = parseInt(localPage, 10);
-  const rowsPerPage = parseInt(localRowsPerPage, 10); */
-  /* const offset = page * rowsPerPage;
-  const count = ids.length || 0; */
+  const ids = cart.fileIds ? cart.fileIds : [];
+  const defaultSortDirection = cart.sortDirection === '' ? table.defaultSortDirection || 'asc' : cart.sortDirection;
+  const CART_QUERY = defaultSortDirection === 'desc' ? GET_MY_CART_DATA_QUERY_DESC : GET_MY_CART_DATA_QUERY;
 
   const { loading, error, data } = useQuery(CART_QUERY, {
-    variables: {
-      // offset,
-      // first: count < rowsPerPage ? count : rowsPerPage,
-      sort_direction: cart.sortDirection,
-      order_by: cart.sortColumn === '' ? table.defaultSortField || '' : cart.sortColumn,
-      file_ids: ids,
-    },
+    variables: { file_ids: ids, order_by: cart.sortColumn === '' ? table.defaultSortField || '' : cart.sortColumn },
   });
+
   if (loading) {
     return (
-      <CartView
-        isLoading
-        data="undefined"
-        defaultSortCoulmn={defaultSortColumnValue}
-        defaultSortDirection={defaultSortDirection}
-      />
+      <CartView isLoading data="undefined" />
     );
   }
 
   if (error || !data) {
     return (
-      <Typography
-        variant="headline"
-        color="error"
-        size="sm"
-      >
-        {error && `An error has occurred in loading CART : ${error}`}
-      </Typography>
+      <Typography variant="headline" color="error" size="sm">{error && `An error has occurred in loading CART : ${error}`}</Typography>
     );
   }
 
-  return (
-    <CartView
-      isLoading={false}
-      fileIDs={ids}
-      updateSortOrder={updateSortOrder}
-      defaultSortCoulmn={defaultSortColumnValue}
-      defaultSortDirection={defaultSortDirection}
-      paginationAPIField={table.paginationAPIField}
-      tableDownloadCSV={table.tableDownloadCSV}
-      localPage={localPage}
-      localRowsPerPage={localRowsPerPage}
-      data={
-        data.filesInList === null || data.filesInList === '' ? [] : data.filesInList
-      }
-    />
-  );
-}
-
-/*
   return (
     <CartView
       isLoading={false}
@@ -91,5 +42,6 @@ export default function cartController() {
         }
     />
   );
+};
 
-export default cartController; */
+export default cartController;
