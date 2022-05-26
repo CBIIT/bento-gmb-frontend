8f864d153e84979b9b2a7cd4f2021bd62b707598
Steven Ogwuazor <95305471+ogwuazors@users.noreply.github.com>
2022-05-26 13:25:25 -0400

    Update FooterView.js

--- src/components/Footer/FooterView.js
+++ src/components/Footer/FooterView.js
@@ -5,28 +5,6 @@ import env from '../../utils/env';
 
 const FILE_SERVICE_API = env.REACT_APP_FILE_SERVICE_API;
 
-// const ICDCFooter = () => {
-//   const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
-//
-//   useEffect(async () => {
-//     const fetchFileService = () => fetch(`${FILE_SERVICE_API}version`)
-//       .then((resp) => (resp.json())).catch(() => ({ version: '' }));
-//
-//     const [fileServiceRes, backendServiceRes] = await Promise.all([
-//       fetchFileService(),
-//       getBackendVersion()]);
-//     const fileServData = fileServiceRes;
-//     setFooterUpdatedData({
-//       ...FooterData,
-//       ...{ FileServiceVersion: fileServData.version || '' },
-//       ...{ BEversion: backendServiceRes['Bento API Version']
-//       || backendServiceRes.version || '' },
-//     });
-//   }, [FooterData]);
-//
-//   return <><Footer data={footerUpdatedData} background={FooterData.bg} /></>;
-// };
-
 const ICDCFooter = () => {
   const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
   const url = window.location.href;
