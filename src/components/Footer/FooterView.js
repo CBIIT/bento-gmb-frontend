c70382b9da0be561101173458d1e02ee951df39b
ogwuazors <95305471+ogwuazors@users.noreply.github.com>
2022-05-26 11:39:50 -0400

    Footer Fix

--- src/components/Footer/FooterView.js
+++ src/components/Footer/FooterView.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Footer } from 'bento-components';
-import FooterData/* , { getBackendVersion } */from '../../bento/globalFooterData';
+import FooterData from '../../bento/globalFooterData';
 import env from '../../utils/env';
 
 const FILE_SERVICE_API = env.REACT_APP_FILE_SERVICE_API;
+
+// const ICDCFooter = () => {
+//   const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
+//
+//   useEffect(async () => {
+//     const fetchFileService = () => fetch(`${FILE_SERVICE_API}version`)
+//       .then((resp) => (resp.json())).catch(() => ({ version: '' }));
+//
+//     const [fileServiceRes, backendServiceRes] = await Promise.all([
+//       fetchFileService(),
+//       getBackendVersion()]);
+//     const fileServData = fileServiceRes;
+//     setFooterUpdatedData({
+//       ...FooterData,
+//       ...{ FileServiceVersion: fileServData.version || '' },
+//       ...{ BEversion: backendServiceRes['Bento API Version']
+//       || backendServiceRes.version || '' },
+//     });
+//   }, [FooterData]);
+//
+//   return <><Footer data={footerUpdatedData} background={FooterData.bg} /></>;
+// };
 
 const ICDCFooter = () => {
   const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
@@ -31,12 +53,12 @@ const ICDCFooter = () => {
         .catch(() => ({ version: '' }));
       const data = response.json();
       const beVersion = await getBEVersion();
-      setFooterUpdatedData({ ...FooterData, ...{ FileServiceVersion: data.version || '' }, ...{ BEversion: beVersion } });
+      setFooterUpdatedData({ ...FooterData, ...{ FileServiceVersion: data.version || '0.0.0' }, ...{ BEversion: beVersion } });
     };
     getSystems();
   }, [FooterData]);
 
-  return <><Footer data={footerUpdatedData} /></>;
+  return <><Footer data={footerUpdatedData} background={FooterData.bg} /></>;
 };
 
 export default ICDCFooter;
