6d363ed151ba55d7ada2e618e50aadf4b142ea0b
huuaho <32457341+huuaho@users.noreply.github.com>
2023-03-29 14:53:28 -0400

    disabled auth

--- src/bento/siteWideConfig.js
+++ src/bento/siteWideConfig.js
@@ -2,7 +2,7 @@ export default {
   // Suggested for replaceEmptyValueWith: 'N/A' or '-' or ''
   replaceEmptyValueWith: '',
   // Enable authenication
-  enableAuthentication: true,
+  enableAuthentication: false,
   // List for options for authentication empty array defaults to google
   authProviders: ['google', 'nih', 'loginGov'],
 };
@@ -13,8 +13,8 @@ export const adminPortal = '/admin';
 export const userProfileRoute = '/profile';
 
 // Public Level Access
-export const PUBLIC_ACCESS = 'None';
+export const PUBLIC_ACCESS = 'Metadata-Only';
 
 // Node level access
-export const NODE_LEVEL_ACCESS = false;
+export const NODE_LEVEL_ACCESS = true;
 export const NODE_LABEL = 'Study Arm(s)';
